4f6b4b601db50725cd89a090ced1461938118c36
Tee <teetat2541@hotmail.com>
2025-07-08 13:56:20 +0700

    edit direction in product page

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -64,7 +64,7 @@ const ItemPage = ({ params }: any) => {
             <dd className="mt-3 text-xl leading-6 sm:col-span-2 sm:mt-0">{item.buildingType}</dd>
           </div>
           <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-center text-xl font-medium leading-6">ประเภทอาคาร</dt>
+            <dt className="text-center text-xl font-medium leading-6">ทิศ</dt>
             <dd className="mt-3 text-xl leading-6 sm:col-span-2 sm:mt-0">{item.direction}</dd>
           </div>
           <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
